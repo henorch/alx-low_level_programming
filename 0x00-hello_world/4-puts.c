6ed6fb32f1f3b8a79c252128f0df28a62cf40059
henorch <henorch01@gmail.com>
2022-10-13 23:13:18 +0100

    my greater mandatory

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/*
+ * main is the entry point of this program
+ *This program print a sentence to standard output using printf
+ the return value of the programe is 0**/
 
 int main(void)
 {
