52665b7587566e6761eedf8b832966bd01893419
henorch <henorch01@gmail.com>
2022-10-14 00:42:28 +0100

    my greater advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  *main-Serves as the entry point of th program
  *
- *Return: The default return value is zero
+ *Return: The programme return value is 1
  */
 
 int main(void)
 {
-fprintf(stderr, "\"and that piece of art is useful -
-Dora Korpar\", 2015-10-19\n");
+write(2, "and that piece of art
+	is usefull\" -Dora Korpar,
+	2015-10-19\n", 59);
 return (1);
 }
