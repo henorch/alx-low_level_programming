2a2b3637ff5ce51fa5e2648b7eca68dadd4fda64
henorch <henorch01@gmail.com>
2022-10-14 05:53:46 +0100

    final part

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  *main-Serves as the entry point of th program
  *
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\"- Dora Korpar,
-2015-10-19\n");
+write(2, "and that piece of art is useful\" -Dora Korpar,
+2015-10-19\n", 59);
 return (1);
 }
