e8ddba609b78302f219e99b859a86438ff32eec0
henorch <henorch01@gmail.com>
2022-10-20 05:55:30 -0700

    finer commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main- This print alphabet in lowwercase
+ *
+ *Return: return the zero default value;
+ */
+
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; ++i)
+{
+printf("%c ", i);
+}
+return (0);
+}
