27ab8cd555b2801a4841fd320dc08b4ad286b96a
henorch <henorch01@gmail.com>
2022-10-20 08:23:37 -0700

    more functions commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_isupper- check if the input is upper
+ *@c: this hold a variable
+ *Return: This return the value of 1 or 0
+ */
+
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (0);
+}
+else
+{
+return (1);
+}
