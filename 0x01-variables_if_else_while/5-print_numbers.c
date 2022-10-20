ca839e5718278f7bc5a17de0aee0797c7a76dd4d
henorch <henorch01@gmail.com>
2022-10-20 06:43:38 -0700

    4 finer commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main- The entrance to my task
+ *
+ *Return: returns default 0 value
+ */
+
+int main(void)
+{
+int i;
+for (i = 1; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
