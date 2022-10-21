edd076102d1c758c3f4fd581411dd53cd24af0e7
henorch <henorch01@gmail.com>
2022-10-21 06:11:33 -0700

    multi commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul- This multiply two numbers
+ *@c: It holds the first variable
+ *@d: It holds the second variable
+ *Return: It return the mul val
+ */
+
+int mul(int c, int d)
+{
+return (c * d);  
+}
