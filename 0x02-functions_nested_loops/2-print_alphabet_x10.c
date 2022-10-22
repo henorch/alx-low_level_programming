8d8f1757bd26780c157ebf7c3f7937bd85bf097e
henorch <henorch01@gmail.com>
2022-10-22 08:18:42 -0700

    my commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet- entry
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+char i;
+int j = 1;
+while (j <= 10)
+{	
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+j++;
+_putchar('\n');
+}
+}
