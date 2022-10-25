9a1543aebea9da1d607b8fe40cd2df42628fca4d
henorch <henorch01@gmail.com>
2022-10-25 03:10:40 -0700

    another commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,19 +12,23 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of ");
-putchar(n);
+printf("Last digit of %d ", n);
+n = n % 10;
 if (n > 5)
 {
 printf("is %d and is greater than 5\n", n);
 }
-else if (n == 0)
-{
-printf("is %d and is zero\n", n);
-}
 else if (n < 6 && n != 0)
 {
-printf("is %d and is less than 6 and not 0\n", n);
+printf("is %d and is less than 6 and not 0\n", n); 
+}
+else if (n == 0)
+{
+printf("is %d and is zero\n", n); 
+}
+else
+{
+printf("\n");
 }
 return (0);
 }
---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main- enterance
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+int i, j;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = '0'; j <= '9'; j++)
+{
+if (!(ij = ji))
+{
+putchar(i);	
+putchar(j);
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
