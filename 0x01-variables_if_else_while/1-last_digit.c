5a4945da35ac842fb66c2c0f7caeb64575e538be
henorch <henorch01@gmail.com>
2022-10-25 03:11:50 -0700

    another commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ printf("is %d and is greater than 5\n", n);
 }
 else if (n < 6 && n != 0)
 {
-printf("is %d and is less than 6 and not 0\n", n); 
+printf("is %d and is less than 6 and not 0\n", n);
 }
 else if (n == 0)
 {
-printf("is %d and is zero\n", n); 
+printf("is %d and is zero\n", n);
 }
 else
 {
