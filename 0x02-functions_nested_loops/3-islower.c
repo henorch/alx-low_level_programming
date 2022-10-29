50bb9fc811569d62c3429fe1bf94aceebc697d6d
henorch <henorch01@gmail.com>
2022-10-29 04:27:50 -0700

    my latest commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
- * print_alphabet- entry
- *
- * Return: void
+ *_islower- entry
+ *@c: the variable
+ * Return: 0
  */
 
-int print_alphabet(int c)
+int _islower(int c)
 {
-if(c >= 'a'; c <= 'z')
+if (c >= 'a' && c <= 'z')
 {
 return (1);
 }
 else
 {
+return (0);
+}
 return (0);
 }
