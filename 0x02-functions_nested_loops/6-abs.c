0c5abe47914f4ee246e49489384b8515dc3cab93
henorch <henorch01@gmail.com>
2022-10-31 05:55:34 -0700

    my new commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs- This print absolute number
+ * @r: The variable
+ * Return: int
+ */
+
+int _abs(int r)
+{
+if (r > 0)
+{
+return (r);
+}
+else
+{
+r = -1 * r;
+return (r);
+}
+return (r);
+}
