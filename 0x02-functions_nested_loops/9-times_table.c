61c5b0041aac0e0e4dbbbeaca5241f2c7707257f
henorch <henorch01@gmail.com>
2022-11-04 17:37:30 -0700

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,10 @@
 #include "main.h"
+
 /**
- *
- *  * times_table - prints the 9 times table
- *
- *   * Description: prints the 9 times table
- *
- *    * Return: void
- *
- *     */
+ * times_table - prints the 9 times table
+ * Description: prints the 9 times table
+ * Return: void
+ */
 
 
 
